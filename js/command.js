9a3127b7aea0e2b1c077f6cce5f78c47552e4377
jung230 <tmdnlxlgjsl@naver.com>
2024-07-08 17:50:00 +0900

    first commit

---
+++ js/command.js
@@ -0,0 +1,44 @@
+$(function(){
+
+    // 메뉴
+
+    $(".main li , .sub_bg").mouseover(function(){
+        $(".sub , .sub_bg").stop().slideDown();
+    })//mouseover
+
+    $(".main li , .sub_bg").mouseout(function(){
+        $(".sub, .sub_bg").stop().slideUp();
+    })//mouseout
+
+
+    // 이미지 영역
+    $(".fade li").eq(0).siblings().hide();
+
+    var n = 0; // 0 1 2
+
+    setInterval(function(){
+
+        // $(".fade li").eq(n).fadeOut();
+        
+        if(n == 2){
+            n=0;
+        }else{ n++;
+        }//
+
+        // $(".fade li").eq(n).fadeIn();
+        
+        $(".fade li").eq(n).fadeIn().siblings().fadeOut();
+        
+        console.log(n);
+
+    } , 2500);
+
+
+
+
+
+
+
+
+
+}) //jquery end
