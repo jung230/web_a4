c2bf79e3302970c867aa0152f7f97718519be117
jung230 <tmdnlxlgjsl@naver.com>
2024-07-09 17:38:36 +0900

    최종

--- js/command.js
+++ js/command.js
@@ -20,19 +20,33 @@ $(function(){
 
         // $(".fade li").eq(n).fadeOut();
         
+
+        // n == 2는 총 3장 중 마지막 번호
         if(n == 2){
             n=0;
-        }else{ n++;
+            // n=0;은 처음 번호
+        }else{
+            n++;
         }//
 
         // $(".fade li").eq(n).fadeIn();
-        
+
         $(".fade li").eq(n).fadeIn().siblings().fadeOut();
         
         console.log(n);
 
     } , 2500);
 
+
+    //팝업
+
+    $(".p_up").click(function(){
+        $(".popup").show();
+    })//
+
+    $(".close").click(function(){
+        $(".popup").hide();
+    })//
 
 
 
